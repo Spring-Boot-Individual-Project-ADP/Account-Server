3926037b15f4e1b797e58fb4655badcc3db13c5f
Ruth Blackman <ruthkjblackman27@gmail.com>
2024-10-14 10:45:17 +0100

    basic project setup

---
+++ src/main/java/com/auth/Spring_Boot_Account_Server/SpringBootAccountServerApplication.java
@@ -0,0 +1,13 @@
+package com.auth.Spring_Boot_Account_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAccountServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAccountServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/Spring_Boot_Account_Server/SpringBootAccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.Spring_Boot_Account_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAccountServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
